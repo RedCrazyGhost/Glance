a6a82c855a03527bdef3e4a33b006dfc582d5f08
Ghost <zhan940842903@163.com>
2023-02-18 01:02:25 +0800

    v0.0.6:add CBBParser

--- core/parser.go
+++ core/parser.go
@@ -6,11 +6,15 @@
 
 package core
 
+// Parser 解析模版接口
+// layout 时间解析样本
+// matchingIndex 匹配数组所在的index位置
 type Parser interface {
 	layout() string
 	matchingIndex() map[string]interface{}
 }
 
+// AlipayParser 支付宝账单解析模版
 type AlipayParser struct {
 }
 
@@ -30,4 +34,22 @@ func (p AlipayParser) matchingIndex() map[string]interface{} {
 	return m
 }
 
-// todo 通过反射执行不同的方法完成赋值
+// CCBParser 中国建设银行账单解析模版
+type CCBParser struct {
+}
+
+// layout 时间解析样本
+func (p CCBParser) layout() string {
+	return "20060102"
+}
+
+// matchingIndex 匹配数组所在的index位置
+func (p CCBParser) matchingIndex() map[string]interface{} {
+	m := make(map[string]interface{})
+	m["Annotation"] = []string{"序号", "对方账号与户名"}
+	m["DateTime"] = "交易日期"
+	m["Target"] = 8
+	m["TradeChannel"] = []string{"交易地点/附言", "对方账号与户名"}
+	m["TradeAmount"] = 5
+	return m
+}
