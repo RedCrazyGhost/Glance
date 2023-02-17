035cdb1a09a4b516cb1438dc45f5e0ad0c31cddf
Ghost <zhan940842903@163.com>
2023-02-18 01:05:58 +0800

    v0.0.6:remove TradeNode struct annotation

--- core/parser_test.go
+++ core/parser_test.go
@@ -19,20 +19,4 @@ func TestAlipayParse(t *testing.T) {
 		fmt.Println(typeOfTradeNode.Field(i))
 	}
 
-	/** 运行结果：
-	{Id  string  0 [0] false}
-	{LBalance  float64  16 [1] false}
-	{Cash  float64  24 [2] false}
-	{Datetime  time.Time  32 [3] false}
-	{Target  string  56 [4] false}
-	{TradeType  core.TradeType  72 [5] false}
-	{TradeAmount  float64  80 [6] false}
-	{TradeChannels  []core.TradeChannel  88 [7] false}
-	{TradeTags  []core.TradeTag  112 [8] false}
-	{Annotation  string  136 [9] false}
-	{Parser  core.Parser  152 [10] false}
-	{Meta  []string  168 [11] false}
-	{Last  *core.TradeNode  192 [12] false}
-	{Next  *core.TradeNode  200 [13] false}
-	**/
 }
